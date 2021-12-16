01e32de55217261bfb511a2a947d2fbb47091614
Nicolai <nicolai_stein@outlook.de>
2021-12-16 19:59:21 +0100

    added first pattern

--- patterns/pattern_parent.py
+++ patterns/pattern_parent.py
@@ -1,23 +1,24 @@
 from typing import Mapping
 import abc
 
-def getCmdParam(label:str, val:float):
-        return " "+ label + str(val) if val != 0.0 else ""
-    
+
+def getCmdParam(label: str, val: float):
+    return " " + label + str(val) if val is not None else ""
+
 
 class PatternParent:
-    def __init__(self, values:Mapping):
+    def __init__(self, values: Mapping):
         self.values = values
         self.result = ""
 
     def reset(self):
-        self.result=""
+        self.result = ""
 
-    def add(self, cmd:str):
+    def add(self, cmd: str):
         self.result += cmd + "\n"
 
-    def addCmd(self, prefix:str, x=0.0, y=0.0, z=0.0, i=0.0, j=0.0):
-        cmd:str = ""
+    def addCmd(self, prefix: str, x=None, y=None, z=None, i=None, j=None):
+        cmd: str = ""
         cmd += getCmdParam("X" + x)
         cmd += getCmdParam("Y" + y)
         cmd += getCmdParam("Z" + z)
@@ -26,24 +27,25 @@ class PatternParent:
         if(cmd):
             self.add(prefix + cmd)
 
-    def moveTo(self, x=0.0, y=0.0, z=0.0):
+    def moveTo(self, x=None, y=None, z=None):
         self.addCmd("G0", x, y, z)
 
-    def printTo(self, x=0.0, y=0.0, z=0.0):
+    def printTo(self, x=None, y=None, z=None):
         self.addCmd("G1", x, y, z)
 
-    def clockArc(self, x=0.0, y=0.0, i=0.0, j=0.0):
+    def clockArc(self, x=None, y=None, i=None, j=None):
         self.addCmd("G02", x, y, i=i, j=j)
-    
-    def counterClockArc(self, x=0.0, y=0.0, i=0.0, j=0.0):
+
+    def counterClockArc(self, x=None, y=None, i=None, j=None):
         self.addCmd("G03", x, y, i=i, j=j)
 
     def relativeMode(self):
         self.add("G91")
-    
+
     def absoluteMode(self):
         self.add("G90")
-    
+
     @abc.abstractmethod
-    def gcode(self, startX:float, startY:float, workHeight:float):
+    def gcode(self, startX: float, startY: float, workHeight: float):
         """Generates the gcode of the pattern"""
+        return
