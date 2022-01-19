3df3050e925f2c3eb1ff413b159233257b4cf2a9
Nicolai <nicolai_stein@outlook.de>
2022-01-19 16:32:35 +0100

    implemented iterative packing

--- gui/canvas/packer.py
+++ gui/canvas/packer.py
@@ -5,31 +5,16 @@ import gui.canvas.translator as translator
 import sys
 from logger import log
 
-
-def pack(shapes):
-    rectangles = []
-    for s in shapes:
-        rectangles.append(shapeToRectangle(s))
-
-    xTotal = yTotal = 0
-
-    for x, y in rectangles:
-        xTotal += x
-        yTotal += y
-
-    log("x total: " + str(xTotal))
-    log("y total: " + str(yTotal))
-
-    maxT = max([xTotal, yTotal])
-    minT = min([xTotal, yTotal])
-    log("min: " + str(minT) + ", max: " + str(maxT))
-
-    bins = [(minT, maxT)]
-
+def calc(rects, size):
     packer = newPacker(sort_algo=SORT_DIFF, rotation=False, pack_algo=SkylineBl)
 
+    bins = [(float2dec(r[0], 3), float2dec(r[1], 3)) for r in [(size, size)]]
+    bins = [(size,size)]
+
+    dec_rects = [(float2dec(r[0], 3), float2dec(r[1], 3)) for r in rects]
+    dec_rects = rects
     # Add the rectangles to packing queue
-    for index, r in enumerate(rectangles):
+    for index, r in enumerate(dec_rects):
         packer.add_rect(*r, rid=index)
 
     # Add the bins where the rectangles will be placed
@@ -39,9 +24,45 @@ def pack(shapes):
     # Start packing
     packer.pack()
 
-    all_rects = packer.rect_list()
-    log("All rects result: " + str(all_rects))
-    return all_rects
+    return packer.rect_list()
+
+def pack(shapes):
+    rectangles = []
+    for s in shapes:
+        rectangles.append(shapeToRectangle(s))
+
+    xTotal = yTotal = 0
+    allWidths = []
+    allHeights = []
+
+    for x, y in rectangles:
+        xTotal += x
+        allWidths.append(x)
+        yTotal += y
+        allHeights.append(y)
+
+    log("x total: " + str(xTotal))
+    log("y total: " + str(yTotal))
+
+    maxT = max([xTotal, yTotal])
+    minT = min([xTotal, yTotal])
+    log("min: " + str(minT) + ", max: " + str(maxT))
+
+
+    allWidths.extend(allHeights)
+    size = max(allWidths)
+    step = size * 0.2
+
+    result = []
+    counter = 0
+    while len(result) != len(rectangles):
+        log("packer calc counter: " + str(counter))
+        result = calc(rectangles, size)
+        size += step
+        counter += 1
+
+    log("All rects result: " + str(result))
+    return result
 
 def shapeToRectangle(vertices: List[List[float]]):
     maxX = maxY = 0
