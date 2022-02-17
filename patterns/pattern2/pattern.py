d4de5e3b15e89a8fc35aae2e6f60e2a67361553a
Nicolai <nicolai_stein@outlook.de>
2022-02-17 21:02:15 +0100

    adjusted radius for patterns 2 and 3

--- patterns/pattern2/pattern.py
+++ patterns/pattern2/pattern.py
@@ -14,6 +14,8 @@ class Pattern(PatternParent):
         b1 = self.values["b1"]
         b2 = self.values["b2"]
         r = b2 / 6
+        printingBetweenArcsNeeded = 4*r+b1 < b2
+        rBottom = r if printingBetweenArcsNeeded else (b2-b1)/4
 
         self.reset()
 
@@ -21,10 +23,11 @@ class Pattern(PatternParent):
         self.workHeight()
 
 
-        self.printTo(x=l1-r)
-        self.clockArc(x=l1, y=(b2-b1)/2-r, j=r*-1, arcDegrees=90)
-        self.printTo(y=r)
-        self.counterClockArc(x=l1+r,y=0, i=r, arcDegrees=90)
+        self.printTo(x=l1-rBottom)
+        self.clockArc(x=l1, y=(b2-b1)/2-rBottom, j=rBottom*-1, arcDegrees=90)
+        if printingBetweenArcsNeeded:
+            self.printTo(y=rBottom)
+        self.counterClockArc(x=l1+rBottom,y=0, i=rBottom, arcDegrees=90)
 
 
         self.printTo(x=l1+l2-r)
@@ -35,12 +38,13 @@ class Pattern(PatternParent):
         self.printTo(x=l1+l2-r)
 
         self.counterClockArc(y=r*6, j=r)
-        self.printTo(x=l1+r)
+        self.printTo(x=l1+rBottom)
 
 
-        self.counterClockArc(x=l1, y=b2-r, j=r*-1, arcDegrees=90)
-        self.printTo(y=(b2-b1)/2 +b1+r)
-        self.clockArc(x=l1-r, y=(b2-b1)/2 +b1, i=r*-1, arcDegrees=90)
+        self.counterClockArc(x=l1, y=b2-rBottom, j=rBottom*-1, arcDegrees=90)
+        if printingBetweenArcsNeeded:
+            self.printTo(y=(b2-b1)/2 +b1+rBottom)
+        self.clockArc(x=l1-rBottom, y=(b2-b1)/2 +b1, i=rBottom*-1, arcDegrees=90)
         self.printTo(x=0)
         self.freeMoveHeight()
         
